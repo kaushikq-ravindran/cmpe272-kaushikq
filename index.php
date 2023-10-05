4e0a4d4ab9390601709629f8061090eeeb83624e
Kaushikq Ravindran <46593242+kaushikq-ravindran@users.noreply.github.com>
2023-10-04 20:51:16 -0700

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,14 @@ $query = $connect->query("SELECT * FROM sql-fruits");
     <td>No</td>
     <td>Fruit Name</td>
   </tr>
-<?php
-$no = 1;
-while($row = $query-> fetch_assoc()){
-  echo = "<tr>
-          <td>$no</td>
-          <td{$row['name']}></td>
-        </tr>";
-        $no++;
-}  
-?>
+  <?php
+  $no = 1;
+  while($row = $query-> fetch_assoc()){
+    echo = "<tr>
+      <td>$no</td>
+      <td{$row['name']}></td>
+    </tr>";
+    $no++;
+   }  
+   ?>
 </table>
